c923d73267881b564ea2ae3f4614c5e4b00280be
nitesh-20 <nsahu6156@gmail.com>
2024-08-10 23:19:45 +0530

    create helper js and added flights and create crud

--- FlightsAndSearch/src/services/flight-service.js
+++ FlightsAndSearch/src/services/flight-service.js
@@ -1,4 +1,5 @@
 const { FlightRepository, AirplaneRepository } = require("../repository/index");
+const { compareTime } = require("../utils/helper");
 
 class FlightService {
 
@@ -9,6 +10,9 @@ class FlightService {
 
     async createFlight(data) {
         try {
+            if(!compareTime(data.arrivalTime, data.departureTime)) {
+                throw {error: 'Arrival time cannot be less than departure time'}
+            }
             const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
             const flight = await this.flightsRepository.createFlight({
                 ...data, totalSeats:  airplane.capacity 
@@ -41,5 +45,5 @@ module.exports = FlightService;
  * arrivalTime,
  * departureTime,
  * price
- *    -> airplane
+ * TotalSeats -> airplane
 */
